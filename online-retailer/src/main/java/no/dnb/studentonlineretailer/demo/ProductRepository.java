a57db4ab147e6e5c646618ea2614bdfd39b2c196
Andy Olsen <andyo@olsensoft.com>
2021-03-15 20:31:25 +0000

    Update ProductRepository.java

--- online-retailer/src/main/java/no/dnb/studentonlineretailer/demo/ProductRepository.java
+++ online-retailer/src/main/java/no/dnb/studentonlineretailer/demo/ProductRepository.java
@@ -2,9 +2,15 @@ package no.dnb.studentonlineretailer.demo;
 
 public interface ProductRepository {
 
+    // FROM ANDY: It's usually OK for this method to take a "whole" Product object, rather than separate params.
+    // Typically you don't know the "id" until after it has been inserted into the database. I'll discuss this on Tuesday.
     void addProduct(int productId, String productName, double price);
+
     boolean deleteProduct(int productId);
+    
+    // FROM ANDY: It's usually OK for this method to take a "whole" Product object, rather than separate params.
     void updateProduct(int productId, String productName, double price);
+
     Product findProductId(int productId);
 
 }
